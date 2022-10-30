dc85e72a190cfed2c836372e6101ac620328a514
Shikhar <shikharsharma94147023@gmail.com>
2022-10-30 14:44:19 +0530

    Leetcode

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        /*
         long long rev=0,temp=x,digit;
         while(x){//as all neg no(s) will not be palindrome
         digit=x%10;
         rev=rev*10+digit;
         x/=10;   
         }
-        if(temp==rev) return true;
-        return false;
+        // if(temp==rev) return true;
+        // return false;
+        return temp==rev;
+        */
+        //stoi approach
+        //edge case
+        if(x<0)return false;
+        string s=to_string(x);//convert x into strong "x"
+        reverse(s.begin(),s.end());
+        // int num=stoi(s);instead of stoi do stoll(long long)
+        long long num=stoll(s);
+        return num==x;
     }
 };
