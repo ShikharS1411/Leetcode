ea864d43b191b1ae6b094f09ac7fbf80a750655a
Shikhar Sharma <114483834+ShikharS1411@users.noreply.github.com>
2022-11-02 00:29:00 +0530

    Update 1759. Count Number of Homogenous Substrings.cpp

--- 1759. Count Number of Homogenous Substrings.cpp
+++ 1759. Count Number of Homogenous Substrings.cpp
@@ -21,5 +21,18 @@ public:
             ans=(ans+count)%mod;//value will remains the same unitll >=mod
         }
         return ans%mod;
+                // https://youtu.be/1nATf3_viTI
+        //same but better way
+        const int mod=1e9+7;
+        int ans=s.size();//as ek baar toh ayega hi
+        int count=1;
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1]){
+                ans=(ans+count)%mod;
+                count++;
+            }
+            else count=1;
+        }
+        return ans%mod;
     }
 };
