37a0d8479869102a23391ad5a93d6f37b6ab4a62
Shikhar <shikharsharma94147023@gmail.com>
2022-11-01 23:32:33 +0530

    Leetcode

---
+++ 1759. Count Number of Homogenous Substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countHomogenous(string s) {
+        /*
+        //brute force->will give tle
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                if(s[i]==s[j])count++;
+                else break;
+            }
+        }
+        return count;
+        */
+        //optimal appraoch
+        int mod=1e9+7,count=1,ans=1;
+        //as homogenous so can treat it as sorted(for a particular element)
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1])count++;
+            else count=1;//as hr element ka 1 count toh already rhega hi
+            ans=(ans+count)%mod;//value will remains the same unitll >=mod
+        }
+        return ans%mod;
+    }
+};
