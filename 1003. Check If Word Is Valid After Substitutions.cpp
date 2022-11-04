e0d2a9503dd6255ad3b4a2c261b7c5a3ed4d7a6f
Shikhar <shikharsharma94147023@gmail.com>
2022-11-05 00:41:16 +0530

    Leetcode

--- 1003. Check If Word Is Valid After Substitutions.cpp
+++ 1003. Check If Word Is Valid After Substitutions.cpp
@@ -21,5 +21,21 @@ public:
         }
         if(st.empty())return true;//that means we have completely popped abc(s) from the given string s ,i.e the given string can be empty so return true
         return false;
+
+        //2nd approach->iterative(find and substr function)
+        while(s.size()>2){
+            //firstly check if the given sub string exists or not
+            int pos=s.find("abc");//will give 1st index i.e. of a's
+            //when abc is not there it will return -1
+            if(pos==-1)return false;
+            //0th index to pos ind(excluded) tk print krega
+            string s1=s.substr(0,pos);//get a substring till that pos index
+            //pos+3 (included)index se size(excluded) tk print krega i.e till size()-1
+            string s2=s.substr(pos+3,s.size());//get a substring from pos+3 till lenght
+            //get new string i.e of removal of abc
+            s=s1+s2;//can direclty add :D
+        }
+        if(s.size()==0)return true;//that means all removal of abc happend
+        return false;
     }
 };
