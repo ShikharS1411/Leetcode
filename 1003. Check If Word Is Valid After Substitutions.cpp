e0758b6dedc4f9090800227a44bb3b0a2b0abf6c
Shikhar <shikharsharma94147023@gmail.com>
2022-11-04 22:08:23 +0530

    Leetcode

---
+++ 1003. Check If Word Is Valid After Substitutions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            //deal all false cases
+            if(s[i]=='a')st.push(s[i]);
+            else if(s[i]=='b'){
+                //if st.empty() means that 1st cahr is not a ,so b,c wont make sense thus return false;
+                if(st.empty()||st.top()!='a')return false;
+                st.push(s[i]);
+            }
+            else if(s[i]=='c'){
+                if(st.empty()||st.top()!='b')return false;
+                //abc would form in stack now if we push
+                st.pop();//only a remains
+                if(st.empty()||st.top()!='a')return false;
+                st.pop();//abc will get removed
+            }
+            else false;
+        }
+        if(st.empty())return true;//that means we have completely popped abc(s) from the given string s ,i.e the given string can be empty so return true
+        return false;
+    }
+};
