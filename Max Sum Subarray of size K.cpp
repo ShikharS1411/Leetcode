e4e33615fec88252e5dfe017a028b552d803d8d2
Shikhar <shikharsharma94147023@gmail.com>
2022-11-06 14:39:01 +0530

    GFG

--- Max Sum Subarray of size K.cpp
+++ Max Sum Subarray of size K.cpp
@@ -1,1 +1,23 @@
-Max Sum Subarray of size K
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        // code here
+        int i=0,j=0;
+        long sum=0,maxi=INT_MIN;
+        while(j<N){
+            sum+=Arr[j];
+            if(j-i+1<K){
+                j++;
+            }
+            //exact window size
+            else if(j-i+1==K){
+                //uss 1st window ka exact sum
+                maxi=max(maxi,sum);
+                sum-=Arr[i];
+                j++;
+                i++;
+            }
+        }
+        return maxi;
+    }
+}
