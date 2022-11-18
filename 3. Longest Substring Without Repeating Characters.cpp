130fd9573ff5e6d7853b98f85ffa7d327b57abc6
Shikhar <shikharsharma94147023@gmail.com>
2022-11-18 23:50:13 +0530

    Leetcode

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
+    bool isvalid(map<char,int>&mp){
+        for(auto x:mp){
+            if(x.second>1)return false;
+        }
+        return true;
+    }
     int lengthOfLongestSubstring(string s) {
-        
-        unordered_map<char,int>umap;
-        int i=0,j=0,maxi=0;
-        while(j<s.size()){
-            //store count of every char of the substring in a map
-            umap[s[j]]++;
-            if(umap.size()>j-i+1) j++;
-            if(umap.size()==j-i+1){
-                maxi=max(maxi,j-i+1);
-                j++;
-            }
-            else if(umap.size()<j-i+1){
-                while(umap.size()<j-i+1){
-                    umap[s[i]]--;
-                    if(umap[s[i]]==0) umap.erase(s[i]);
-                    i++;
+        /*
+        //1st approach->brute force
+        int maxi=INT_MIN;
+        for(int i=0;i<s.size();i++){
+            string temp="";
+            set<char>st;
+            int n=temp.size();
+            for(int j=i;j<s.size();i++){
+                if(st.size()==n){
+                    st.insert(s[j]);
+                    temp+=s[j];   
+                    int n=temp.size();
+                    if(st.size()!=n)maxi=max(maxi,n-1);
                 }
-                j++;
+                else break;   
             }
+            maxi=max(maxi,n);
         }
         return maxi;
+        */
+        //2nd optimal approach->using sliding window/two pointer(as size related q) and hashmaps
+        map<char,int>mp;
+        int i=0,j=0,n=s.size(),ans=0;//better do 0 as empty string can also be there
+        while(j<n){
+            mp[s[j]]++;
+            if(isvalid(mp))ans=max(ans,j-i+1);//j-i+1->size of window/substring
+            //jb reapeating char aa jye
+            else{
+                while(!isvalid(mp) and i<j){
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0)mp.erase(s[i]);
+                    i++;
+                }
+                ans=max(ans,j-i+1);
+            }
+            j++;
+        }
+        return ans;
     }
 };
