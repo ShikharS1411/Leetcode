ca73dffc2d1704121205a5e792b9f0d1e9acf4d6
Shikhar <shikharsharma94147023@gmail.com>
2022-12-04 00:52:45 +0530

    Leetcode

--- 1636. Sort Array by Increasing Frequency.cpp
+++ 1636. Sort Array by Increasing Frequency.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        // by using max heap
+        /*
+        //1st approach-> by using max heap
         unordered_map<int,int>umap;
         for(auto x:nums)umap[x]++;
         priority_queue<pair<int,int>>maxh;
@@ -19,5 +20,14 @@ public:
             maxh.pop();
         }
     return temp;
+        */
+        //2nd approach->by using lamda function
+        unordered_map<int, int>umap;
+        for (auto x: nums)umap[x]++;
+        //lamda func
+        sort(nums.begin(),nums.end(),[&](int a, int b) {
+            return umap[a]==umap[b]?a>b:umap[a]<umap[b];
+        });
+        return nums;
     }
 };
