c2dbd0a8fb76456fc9a788c4399ad971df5b0384
Shikhar <shikharsharma94147023@gmail.com>
2022-12-13 17:05:12 +0530

    Leetcode

--- 205. Isomorphic Strings.cpp
+++ 205. Isomorphic Strings.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        /*
         //edge case
         if(s.size()!=t.size())return false;
         map<char,char> mp;
@@ -9,5 +10,19 @@ public:
         string res=s;
         for(auto &x:res)x=mp[x];
         return res==t;
+        */
+        unordered_map<char,char>mp;
+        int n=s.size();
+        int m=t.size();
+        if(n!=m) return false;
+        for(int i=0;i<n;i++){
+           if(mp.find(s[i])!=mp.end()&&mp[s[i]]!=t[i]) return false;
+            mp[s[i]]=t[i];
+        }
+        set<char>st;
+        for(auto i:mp){
+        st.insert(i.second);}
+        if(mp.size()!=st.size()) return false;
+        return true;
     }
 };
