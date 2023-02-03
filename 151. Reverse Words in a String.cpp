9aad8e43a02c66b1e98d55b39f6691079074f88a
Shikhar <shikharsharma94147023@gmail.com>
2023-02-03 16:46:57 +0530

    Leetcode

--- 151. Reverse Words in a String.cpp
+++ 151. Reverse Words in a String.cpp
@@ -1,43 +1,19 @@
+#define pb push_back
 class Solution {
 public:
     string reverseWords(string s) {
-        /*
-        stack<string>st;
+        string t="";   
+        vector<string>ans;
         for(int i=0;i<s.size();i++){
-            string temp="";//empty string and will be created every time when the loop will end
-            while(s[i]!=' '&&i<s.size()){
+            string temp="";
+            while(s[i]!=' ' && i<s.size()){
                 temp+=s[i];
                 i++;
             }
-            //upr wle loop se temp string bnyi aur ab hr temp string ko push kr rhe
-            if(temp!="")st.push(temp);
+            if(temp!="")ans.pb(temp);
         }
-        string ans="";//for return type
-        while(!st.empty()){
-            if(!ans.size())ans=st.top();
-            else ans+=" "+st.top();
-            st.pop();
-        }
-        return ans;
-        */
-        /*
-        //2nd appraoch->using stringstream
-        //stringstream is use to count the number of words in a string
-        stringstream ss(s);
-        //for iteration we'll use a temp string
-        string word="",ans="";
-        stack<string>st;
-        while(ss>>word)st.push(word);
-        while(!st.empty()){
-            ans+=st.top()+" ";
-            st.pop();
-        }
-        return ans.substr(0,ans.size()-1);//to avoid the last " "
-        */
-        //more optimised of 2nd one
-        stringstream all(s); 
-        string word,ans = "";
-        while (all >> word)ans = word + " " + ans;//will be reversed as adding ans aftr adding word to it
-        return ans.substr(0,ans.length()-1);//to avoid that " " in the end
+        reverse(ans.begin(),ans.end());
+        for(auto x:ans)t+=x+' ';
+        return t.substr(0,t.size()-1);
     }
 };
