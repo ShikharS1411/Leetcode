3f0e431429c33108c308c5be3a8b80c0db87eb87
Shikhar <shikharsharma94147023@gmail.com>
2023-03-11 21:47:27 +0530

    Leetcode

--- 438. Find All Anagrams in a String.cpp
+++ 438. Find All Anagrams in a String.cpp
@@ -2,6 +2,8 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        /*
+        not optimal approach idk y
         //sliding window approach
         sort(p.begin(),p.end());
         int n=p.size();
@@ -18,6 +20,24 @@ public:
             temp+=s[j];
             temp.erase(0,1);
             i++,j++;
+        }
+        return ans;
+        */
+        vector<int>ans;
+        int n=p.size();
+        //dont use sorting go with map instead kekw
+        map<char,int>mp1,mp2;
+        for(int i=0;i<n;i++){
+            mp1[s[i]]++;
+            mp2[p[i]]++;
+        }
+        int i=0,j=n-1;
+        while(j<s.size()){
+            if(mp1==mp2)ans.push_back(i);
+            if(mp1[s[i]]>1)mp1[s[i]]--;
+            else mp1.erase(s[i]);
+            i++,j++;
+            mp1[s[j]]++;
         }
         return ans;
     }
