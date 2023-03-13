05092dc054f7ecb060d6d64d6bc90e9de2629e07
Shikhar <shikharsharma94147023@gmail.com>
2023-03-13 22:44:35 +0530

    Leetcode

--- 1839. Longest Substring Of All Vowels in Order.cpp
+++ 1839. Longest Substring Of All Vowels in Order.cpp
@@ -1,6 +1,7 @@
+//prev wrong approach+tle one
+/*
 class Solution {
 public:
-    /*
     bool isvalid(map<char,int>&umap1,map<char,int>&umap2){
         string temp1="",temp2="";
         for(auto x:umap1)temp1+=x.first;
@@ -8,14 +9,11 @@ public:
         return temp1==temp2;
         
     }
-    */
-    /*
+    //for 2nd
     bool isvalid(set<char>&s1,set<char>&s2){
         return s1==s2;
     }
-    */
     int longestBeautifulSubstring(string word) {
-        /* 
         //not appropriate approach
         //manually define a map
         map<char,int>umap1={{'a',1},{'e',1},{'i',1},{'o',1},{'u',1}};
@@ -38,8 +36,9 @@ public:
             umap2[word[j]]++;
         }
         return maxi;
-        */
-        /*
+        
+        //2nd approach 
+        //will give tle 
         //using sets
         set<char>s1={'a','e','i','o','u'},s2;
         //now start traversing
@@ -50,29 +49,49 @@ public:
         }
         for(int j=5;j<n;j++){
             s2.insert(word[j]);
-            //<5 aur !isvalid aur vowels ka bhi dekhna hoga yeh bekr appraoch h
-            while(i<j and !isvalid(s1,s2) and j-i+1==5){//must apply i<j condition
+            while(!isvalid(s1,s2)){
                 s2.erase(word[i]);
                 i++;
             }
-            if(isvalid(s1,s2))maxi=max(maxi,j-i+1);
+            maxi=max(maxi,j-i+1);
         }
         return maxi;
-        */
-        unordered_map<char,int>umap;
-        int i=0,n=word.size(),maxi=0;
-        for(int j=0;j<n;j++){
-            if(umap.empty() and word[j]=='a'){
-                umap[word[j]]++;
-                i=j;//update i
+    }
+};
+*/
+
+
+#define pb push_back
+#define fi first
+#define se second
+class Solution {
+public:
+    int longestBeautifulSubstring(string s) {
+        //implementation based 
+        int n=s.size();
+        //edge case as not in constraints
+        if(n<5)return 0;
+
+        vector<pair<char,int>>vp;
+        int count=1;//wrna last rem ka count nhi hoga 
+        //collective storing
+        //isse last single char ka count store nhi kr pyenge directly so ek extra diff char add kr do
+        s+='#';
+        for(int i=1;i<=n;i++){
+            if(s[i]==s[i-1])count++;
+            else{
+                vp.pb({s[i-1],count});
+                count=1;//update to fresh
             }
-            else if(!umap.empty() and word[j-1]>word[j]){
-                umap.clear();//cant make a substring from itneed to discard completely
-                j--;
-            }
-            else {//when all perfect
-                umap[word[j]]++;
-                if(umap.size()==5)maxi=max(maxi,j-i+1); 
+        }
+        
+        for(auto x:vp)cout<<x.fi<<" "<<x.se<<endl;
+        int maxi=0;//dont do -1e9 as if not found then need to return 0 itself 
+        int sz=vp.size();
+        for(int i=0;i<sz-4;i++){
+            if(vp[i].fi=='a' && vp[i+1].fi=='e' && vp[i+2].fi=='i' && vp[i+3].fi=='o' && vp[i+4].fi=='u' ){
+            int sum=(vp[i].se+vp[i+1].se+vp[i+2].se+vp[i+3].se+vp[i+4].se);
+            maxi=max(maxi,sum);        
             }
         }
         return maxi;
