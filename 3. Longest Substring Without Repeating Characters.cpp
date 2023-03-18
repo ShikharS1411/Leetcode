7d75c9eb82ad404aa84f1662e60b4c6f2274a151
Shikhar <shikharsharma94147023@gmail.com>
2023-03-18 13:05:46 +0530

    Leetcode

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,65 +1,25 @@
+#define fi first
+#define se second
 class Solution {
 public:
-    bool isvalid(map<char,int>&mp){
-        for(auto x:mp){
-            if(x.second>1)return false;
+    bool check(unordered_map<char,int>&umap){
+        for(auto x:umap){
+            if(x.se>1)return false;
         }
         return true;
     }
     int lengthOfLongestSubstring(string s) {
-        /*
-        //1st approach->brute force
-        int maxi=INT_MIN;
-        for(int i=0;i<s.size();i++){
-            string temp="";
-            set<char>st;
-            int n=temp.size();
-            for(int j=i;j<s.size();i++){
-                if(st.size()==n){
-                    st.insert(s[j]);
-                    temp+=s[j];   
-                    int n=temp.size();
-                    if(st.size()!=n)maxi=max(maxi,n-1);
-                }
-                else break;   
-            }
-            maxi=max(maxi,n);
-        }
-        return maxi;
-        */
-        //2nd optimal approach->using sliding window/two pointer(as size related q) and hashmaps
-        /*
-        map<char,int>mp;
-        int i=0,j=0,n=s.size(),ans=0;//better do 0 as empty string can also be there
-        while(j<n){
-            mp[s[j]]++;
-            if(isvalid(mp))ans=max(ans,j-i+1);//j-i+1->size of window/substring
-            //jb reapeating char aa jye
+        //revisited code
+        unordered_map<char,int>umap;
+        int i=0,maxi=0;
+        for(int j=0;j<s.size();j++){
+            umap[s[j]]++;
+            if(check(umap))maxi=max(maxi,j-i+1);
             else{
-                while(!isvalid(mp) and i<j){
-                    mp[s[i]]--;
-                    if(mp[s[i]]==0)mp.erase(s[i]);
-                    i++;
-                }
-                ans=max(ans,j-i+1);
-            }
-            j++;
-        }
-        return ans;
-        */
-        //better code
-        int i=0,n=s.size(),maxi=0;
-        map<char,int>mp;
-        for(int j=0;j<n;j++){
-            mp[s[j]]++;
-            while(!isvalid(mp)){
-                //remove the freq of s[i] and then move the pointer ahead
-                mp[s[i]]--;
-                //check if 0
-                if(mp[s[i]]==0)mp.erase(s[i]);
+                umap[s[i]]--;
+                if(umap[s[i]]==0)umap.erase(s[i]);
                 i++;
             }
-            maxi=max(maxi,j-i+1);
         }
         return maxi;
     }
