36b97d630ddaf302c7edfeba474631e5ab9bf6c3
Shikhar <shikharsharma94147023@gmail.com>
2023-04-15 13:32:18 +0530

    Leetcode

---
+++ 1170. Compare Strings by Frequency of the Smallest Character.cpp
@@ -0,0 +1,31 @@
+#define pb push_back
+#define fi first
+#define se second
+class Solution {
+public:
+    vector<int> numSmallerByFrequency(vector<string>& q, vector<string>& w) {
+        //as per constraints can apply brute force
+        //hashmaps ig
+        map<int,int>mp;
+        int i=1;
+        for(auto x:w){
+            char ch=*min_element(x.begin(),x.end());
+            int cnt=count(x.begin(),x.end(),ch);
+            mp[i++]=cnt;//uniquely iodentify it ez hehe
+        }
+        for(auto x:mp)cout<<x.fi<<" "<<x.se<<"  ";
+        cout<<endl;
+        // return {1};
+        vector<int>ans;
+        for(auto x:q){
+            int cntr=0;
+            char ch=*min_element(x.begin(),x.end());
+            int cnt=count(x.begin(),x.end(),ch);
+            for(auto it:mp){
+                if(it.se>cnt)cntr++;
+            }
+            ans.pb(cntr);
+        }
+        return ans;
+    }
+};
