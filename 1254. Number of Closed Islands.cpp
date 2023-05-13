dcc98c856494e0be8268a2ba79a9e3ede9dccf50
Shikhar <shikharsharma94147023@gmail.com>
2023-05-13 14:35:16 +0530

    Leetcode

--- 1254. Number of Closed Islands.cpp
+++ 1254. Number of Closed Islands.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    void dfs2(int i,int j,int n,int m,vector<vector<int>>& a){
-        if(i<0||j<0||i>=n||j>=m||a[i][j]==1)return;
-        if(a[i][j]==0)a[i][j]=1;//visited
-        //up
-        dfs2(i-1,j,n,m,a);
-        //down
-        dfs2(i+1,j,n,m,a);
-        //left
-        dfs2(i,j-1,n,m,a);
-        //right
-        dfs2(i,j+1,n,m,a);
-    }
     void dfs(int i,int j,int n,int m,vector<vector<int>>& a){
         if(i<0||j<0||i>=n||j>=m||a[i][j]==1)return;
         if(a[i][j]==0)a[i][j]=1;//visited
@@ -37,7 +25,7 @@ public:
             for(int j=0;j<m;j++){
                 if(a[i][j]==0){
                     count++;
-                    dfs2(i,j,n,m,a);
+                    dfs(i,j,n,m,a);
                 }
             }
         }
