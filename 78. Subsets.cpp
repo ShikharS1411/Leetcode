e54f40554a61413728dab7bb7080cf26b22410e1
Shikhar <shikharsharma94147023@gmail.com>
2023-05-16 13:35:22 +0530

    Leetcode

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -1,69 +1,24 @@
+#define pb push_back
+#define ppb pop_back
 class Solution {
-    /*
-    private:
-    void getsets(vector<int>&nums, vector<int> temp, int index,vector<vector<int>>& ans) {    
-        //as subsets/subsequences are asked 
-        //using recurssive approach
-        //as subsequences so pick and not pick method
-        //base case
-        if(index >= nums.size()) {
-            ans.push_back(temp);
-            return ;
-        }
-        
-
-        //take/pick
-        temp.push_back(nums[index]);
-        //function call
-        getsets(nums, temp, index+1, ans);
-        temp.pop_back();
-        //not take/not pick
-        //function call
-        getsets(nums, temp, index+1, ans);
-
-    }
-    //ATQ
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        //define all constraints here
-        vector<vector<int> > ans;//acc.to given subset array as have to return                                       //this only
-        vector<int> temp;//as this is not the array which is supposed to                                    //print/returned
-        int index = 0;
-        getsets(nums, temp, index, ans);
-        return ans;
-        */
-    
-    public:
-        //as subsets/subsequences are asked 
-        //using recurssive approach
-        //as subsequences so pick and not pick method
-    
-    void getsets(vector<int>&nums,int i,vector<int>temp,vector<vector<int>>&ans){
-        
-        //base case
-        if(i>=nums.size()){
-            ans.push_back(temp);
+    void rec(int i,int n,vector<int>& a,vector<int>&temp,vector<vector<int>>&ans){
+        if(i>=n){
+            ans.pb(temp);
             return;
         }
-        
-        //include/ take the element
-        temp.push_back(nums[i]);
-        //function call
-        getsets(nums,i+1,temp,ans);
-        //exclude/pop/not take the element
-        temp.pop_back();
-        //function call
-        getsets(nums,i+1,temp,ans);
-        
-        //[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]
+        //take
+        temp.pb(a[i]);
+        rec(i+1,n,a,temp,ans);
+        temp.ppb();
+        //not take
+        rec(i+1,n,a,temp,ans);
     }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsets(vector<int>& a) {
         vector<vector<int>>ans;
-        vector<int> temp;
-        //function call
-        getsets(nums,0,temp,ans);
-        
+        vector<int>temp;
+        int n=a.size();
+        rec(0,n,a,temp,ans);
         return ans;
     }
 };
