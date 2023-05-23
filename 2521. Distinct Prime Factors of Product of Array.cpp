54258cfb581cbad5934aeace500efb35e3873d58
Shikhar <shikharsharma94147023@gmail.com>
2023-05-24 01:40:26 +0530

    Leetcode

--- 2521. Distinct Prime Factors of Product of Array.cpp
+++ 2521. Distinct Prime Factors of Product of Array.cpp
@@ -1,18 +1,21 @@
 class Solution {
-    void helper(int x,set<int>&st){
-        for(int i=2;i*i<=x;i++){
-            while(x%i==0){
-                st.insert(i);
-                x=x/i;
-            }
-        }
-        //only prime factor left
-        if(x>1)st.insert(x);
-    }
 public:
     int distinctPrimeFactors(vector<int>& a) {
-        set<int>st;
-        for(auto x:a)helper(x,st);
-        return st.size();
+        // set<int>st;
+        map<int,int>mp;
+        for(auto x:a){
+            //get all the primes ez
+            // set<int>st;
+            for(int i=2;i<=x;i++){
+                if(x%i==0){
+                    while(x%i==0)x/=i;
+                    cout<<i<<" ";
+                    // st.insert(i);
+                    mp[i]++;
+                }
+            }
+            cout<<endl;
+        }
+        return mp.size();
     }
 };
