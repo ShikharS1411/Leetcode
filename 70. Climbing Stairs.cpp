82c22b540d81a25bf9f2d7f9a4183fed8a6dfb65
Shikhar <shikharsharma94147023@gmail.com>
2023-06-07 15:15:14 +0530

    Leetcode

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -3,8 +3,7 @@ public:
     int dfs(int n,vector<int>&dp){
         if(n<=2)return n;
         if(dp[n]!=-1)return dp[n];
-        dp[n]=dfs(n-1,dp)+dfs(n-2,dp);
-        return dp[n];
+        return dp[n]=dfs(n-1,dp)+dfs(n-2,dp);
     }
     int climbStairs(int n) {
         //total no of distinct way->DP
