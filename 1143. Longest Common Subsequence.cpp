43cbd06fa3eeaded19d4ab43d8788b33e8e33e75
Shikhar <shikharsharma94147023@gmail.com>
2023-06-15 19:31:57 +0530

    Leetcode

---
+++ 1143. Longest Common Subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dfs(string& s, string& t,int i,int j,vector<vector<int>>&dp){
+        if(i<0||j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==t[j]){
+            int match=1+dfs(s,t,i-1,j-1,dp);
+            dp[i][j]=match;
+            return dp[i][j];
+        }
+        int left=0+dfs(s,t,i-1,j,dp);
+        int right=0+dfs(s,t,i,j-1,dp);
+        int not_match=max(left,right);
+        dp[i][j]=not_match;
+        return dp[i][j]; 
+    }
+    int longestCommonSubsequence(string s,string t) {
+        int n=s.size(),m=t.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return dfs(s,t,n-1,m-1,dp);
+    }
+};
