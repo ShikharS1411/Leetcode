2f2f5b734cda99d8a8f4a4cb1f59c0a35db58984
Shikhar <shikharsharma94147023@gmail.com>
2023-06-17 14:35:53 +0530

    Leetcode

---
+++ 5. Longest Palindromic Substring.cpp
@@ -0,0 +1,30 @@
+//tle/brute force approach
+class Solution {
+public:
+    
+    bool ispalindrome(string x){
+        string temp=x;
+        reverse(x.begin(),x.end());
+        return temp==x;
+    }
+    
+    string longestPalindrome(string s) {
+        
+        // Memory limit exceed :P
+        //can apply brute force as per constraints
+        vector<pair<int,string>>v;
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                if(ispalindrome(s.substr(i,j-i+1))){
+                    v.push_back({j-i+1,s.substr(i,j-i+1)});
+                }
+                // else break;
+            }
+        }
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string a=v[n-1].second;
+        return a;
+        
+    }
+};
