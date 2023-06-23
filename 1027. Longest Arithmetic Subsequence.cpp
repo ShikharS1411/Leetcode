15f9706c3452397566099be089cee6381499c6e6
Shikhar <shikharsharma94147023@gmail.com>
2023-06-23 13:01:02 +0530

    Leetcode

---
+++ 1027. Longest Arithmetic Subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& a) {
+        int ans = 0;
+        vector<vector<int>> dp(1001,vector<int>(1001,0));
+        for(int i=0;i<a.size();i++){
+            for(int j=i-1;j>=0;--j){
+                int dif = a[i]-a[j];
+                dp[i][dif+500] = max(dp[i][dif+500],dp[j][dif+500]+1);
+                ans = max(dp[i][dif+500],ans);
+            }
+        }
+        return ans+1;
+    }
+};
