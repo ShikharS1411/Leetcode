361dfa4c2e44cbf14e13934e5f3fc9ad1f0313ad
Shikhar <shikharsharma94147023@gmail.com>
2023-06-26 11:58:55 +0530

    Leetcode

--- 2435. Paths in Matrix Whose Sum Is Divisible by K.cpp
+++ 2435. Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int dfs(vector<vector<int>>& a, int i,int j,int n,int m,ll sum,int k,vector<vector<vector<int>>> &dp){
         if(i>=n||j>=m)return 0;
-        if(i==n-1 && j==m-1 && (sum+a[i][j])%k==0)return dp[i][j][sum%k]=1;
+        if(i==n-1 && j==m-1 && (sum+a[i][j])%k==0)return 1;
         if(dp[i][j][sum%k]!=-1)return dp[i][j][sum%k];
         //down
         int down=dfs(a,i+1,j,n,m,sum+a[i][j],k,dp);
