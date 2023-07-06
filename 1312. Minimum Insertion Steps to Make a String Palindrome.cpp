00fb74eca2b9a25aff0767a25cbe9b04d24ed605
Shikhar <shikharsharma94147023@gmail.com>
2023-07-06 19:16:38 +0530

    Leetcode

---
+++ 1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dfs(string& s,string& t,int i,int j,vector<vector<int>>&dp){
+        if(i<0||j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==t[j]){
+            int match=1+dfs(s,t,i-1,j-1,dp);
+            dp[i][j]=match;
+            return dp[i][j];
+        }
+        int left=0+dfs(s,t,i-1,j,dp);
+        int right=0+dfs(s,t,i,j-1,dp);
+        int not_match=max(left,right);
+        dp[i][j]=not_match;
+        return dp[i][j]; 
+    }
+    int minInsertions(string s) {
+        // int n=s.size();
+        string t=s;
+        reverse(t.begin(),t.end());
+        //now find longest comm subs
+        int n=s.size(),m=t.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return n-dfs(s,t,n-1,m-1,dp);
+    }
+};
