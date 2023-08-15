b7dcfc5b16d20fe38d30a81eae0cb479cd0a4088
Shikhar <shikharsharma94147023@gmail.com>
2023-08-15 14:58:51 +0530

    CP

---
+++ Permutations tricks for CP.cpp
@@ -0,0 +1,3 @@
+//min dist for a permu can be at max n/2->(floor(n/2))
+//i.e [1,n/2]
+//most of the constructive permu probs (800-1400) will be made by doing some slight changes with n/2 thing , so keep in mind :)
