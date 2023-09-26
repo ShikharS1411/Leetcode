01a576ca814f59936fb0603674e78664dd8e9c69
Shikhar <shikharsharma94147023@gmail.com>
2023-09-26 15:13:21 +0530

    Leetcode

--- 560. Subarray Sum Equals K.cpp
+++ 560. Subarray Sum Equals K.cpp
@@ -1,45 +1,16 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        /*
-        //brute force
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            long long sum=0;
-            //edge case
-            if(nums[i]==k)count++;
-            for(int j=i;j<nums.size();j++){
-                sum+=nums[j];
-                if(sum==k)count++;
-            }
-        }
-        return count;
-        */
-        /*
-        //optimal approach->sliding window but giving tle -_-
-        int i=0,j=0,sum=0,count=0;
-        while(j<nums.size()){
-            sum+=nums[j];//windowsize=j-i+1
-            if(sum<k)j++;
-            else if(sum==k){
-                count++;
-                sum-=nums[i];
-                i++,j++;//cant do this here as not specified that it will contains only +ve integers,so need to make more cases to counter this ,thus tle would be there as well
-            }
-        }
-        return count;
-        */
-        //memorise this approach lol
-        //https://youtu.be/bWym1dWZhh0
-        int n=nums.size();
-        int count=0;
-        unordered_map<int, int> prefsum;
-        int sum=0;
+    int subarraySum(vector<int>& a, int k) {
+        int n=a.size();
+        int pref=0,count=0;
+        map<int,int>mp;
+        //for 0 val of pref when ==k
+        mp[pref]++;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(sum==k)count++;
-            if(prefsum.find(sum-k) != prefsum.end())count+=prefsum[sum-k];
-            prefsum[sum]++;
+            pref+=a[i];
+            int need=pref-k;
+            count+=mp[need];
+            mp[pref]++;
         }
         return count;
     }
