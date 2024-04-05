f0e05520287760fe15ecc76f678049b0f9352733
Shikhar <shikharsharma94147023@gmail.com>
2024-04-05 17:33:55 +0530

    Leetcode

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -28,3 +28,37 @@ public:
         
     }
 };
+
+
+//DP approach
+class Solution {
+public:
+    
+    // bool ispal(string x){
+    //     string temp=x;
+    //     reverse(x.begin(),x.end());
+    //     return temp==x;
+    // }
+    int dp[1001][1001];
+    bool ispal(int i,int j,string &s){
+        //base case
+        if(i>=j)return true;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==s[j])return dp[i][j]=ispal(i+1,j-1,s);
+        return false;
+    }
+    string longestPalindrome(string s) {
+        memset(dp,-1,sizeof(dp));
+        int n=s.size();
+        int ind1=-1,ind2=-1,maxi=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(ispal(i,j,s)){
+                    //do this pal checking recursively with DP
+                    if(j-i+1>maxi){maxi=j-i+1;ind1=i;ind2=j;}
+                }
+            }
+        }
+        return s.substr(ind1,maxi);
+    }
+};
